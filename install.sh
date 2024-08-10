d8c5242aa012b476325d718bcdced63896197336
Halil Teyfik Dolmaci <hlltvfk@gmail.com>
2024-08-10 04:56:44 +0300

    fix: Fix installation script to handle joining an existing cluster
    The installation script was modified to handle joining an existing cluster by checking for the presence of the `K3S_TOKEN` and `K3S_URL` environment variables. If both variables are present, the script will use them to join the cluster. This change ensures that the script can properly handle joining an existing cluster without any issues.

--- install.sh
+++ install.sh
@@ -15,6 +15,16 @@ function prompt() {
   local prompt="$1"
   local varname="$2"
   local default="$3"
+  local should_seed="$4"
+
+  if [ -n "${!varname}" ]; then
+    return
+  fi
+
+  if [ "$should_seed" = "true" ]; then
+    eval export $varname="$default"
+    hint "\"$varname\" will be set to \"$default\""
+  fi
 
   new_line
 
@@ -64,6 +74,8 @@ function get_public_ip() {
     "https://ipecho.net/plain"
   )
 
+  echo "Getting public IP address..."
+
   for provider in "${public_ip_providers[@]}"; do
     public_ip=$(curl -s "$provider" 2>/dev/null || true)
 
@@ -92,7 +104,7 @@ function confirm() {
   local yes
   local no
 
-  if [ "$default" == "y" ]; then
+  if [ "$default" = "y" ]; then
     yes="Y"
     no="n"
   else
@@ -127,47 +139,83 @@ function main() {
   local DEFAULT_RANCHER_PASSWORD="$(dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 | tr -d '/+' | cut -c1-32)"
 
   local confirm
+  local ci="false"
+  local join="false"
 
-  echo "This script will perform the following actions:"
+  if [[ -n "$K3S_TOKEN" && -n "$K3S_URL" ]]; then
+    echo "Your environment exposed K3S_TOKEN and K3S_URL, using them to join the cluster."
 
-  hint "Install required packages for this script to work."
-  hint "Get your public IP address to inform about the DNS A record."
-  hint "Update shell profile to include k3s and Helm aliases."
-  hint "Install k3s and Helm."
+    ci="true"
+    join="true"
 
-  confirm "Do you want to continue?" "y"
+    local master_hostname
 
-  # Prompt for user input
-  echo "Setup Rancher on k3s"
+    if [[ "$K3S_URL" == *"@"* ]]; then
+      master_hostname="$(echo "$K3S_URL" | awk -F[:/@] '{print $6}')"
+    else
+      master_hostname="$(echo "$K3S_URL" | awk -F[:/] '{print $4}')"
+    fi
 
-  hint "Rancher UI will use cert-manager to generate SSL certificates with Let's Encrypt."
-  hint "Let's Encrypt requires an e-mail to be used as a contact in case of issues."
-  hint "Please provide an e-mail address to be used by Let's Encrypt."
+    export RANCHER_HOSTNAME="$master_hostname"
+  fi
 
-  prompt "E-mail" "LETSENCRYPT_EMAIL"
+  if [[ -n "$LETSENCRYPT_EMAIL" && -n "$RANCHER_HOSTNAME" ]]; then
+    echo "Your environment exposed LETSENCRYPT_EMAIL and RANCHER_HOSTNAME, using them to install Rancher."
+    echo "If you set any other environment variables, they will be used as well."
+    echo "Otherwise, default values will be used."
+
+    ci="true"
+  fi
+
+  if [ "$ci" = 'false' ]; then
+    echo "This script will perform the following actions:"
+
+    hint "Install required packages for this script to work."
+    hint "Get your public IP address to inform about the DNS A record."
+    hint "Update shell profile to include k3s and Helm aliases."
+    hint "Install k3s and Helm."
+
+    confirm "Do you want to continue?" "y"
+    new_line
+  fi
+
+  install_deps
+
+  eval $(echo "export KUBECONFIG='/etc/rancher/k3s/k3s.yaml'" | tee -a "$PROFILE_FILE")
+
+  if [ -z "$LETSENCRYPT_EMAIL" ]; then
+    hint "Rancher UI will use cert-manager to generate SSL certificates with Let's Encrypt."
+    hint "Let's Encrypt requires an e-mail to be used as a contact in case of issues."
+    hint "Please provide an e-mail address to be used by Let's Encrypt."
+  fi
+
+  if [ "$ci" = 'false' ]; then
+    prompt "E-mail" "LETSENCRYPT_EMAIL"
+  fi
 
   # Prompt k3s version
-  prompt "K3S version" "K3S_VERSION" "$DEFAULT_K3S_VERSION"
-
-  # Rancher settings
-  new_line
-  echo "Getting public IP address..."
+  prompt "K3S version" "K3S_VERSION" "$DEFAULT_K3S_VERSION" "$ci"
 
   local public_ip="$(get_public_ip)"
 
-  hint "Please provide an FQDN that will be used to access the Rancher UI."
-  hint "Rancher UI will be accessible within this domain."
-  hint "You must setup an A record that points to \"$public_ip\" for this domain."
+  if [ -z "$RANCHER_HOSTNAME" ]; then
+    hint "Please provide an FQDN that will be used to access the Rancher UI."
+    hint "Rancher UI will be accessible within this domain."
+    hint "You must setup an A record that points to \"$public_ip\" for this domain."
+  fi
 
   prompt "Rancher hostname" "RANCHER_HOSTNAME"
-  prompt "Rancher namespace" "RANCHER_NAMESPACE" "$DEFAULT_RANCHER_NAMESPACE"
-  prompt "Rancher Helm repository" "RANCHER_REPO" "$DEFAULT_RANCHER_REPO"
-  prompt "Rancher password" "RANCHER_PASSWORD" "$DEFAULT_RANCHER_PASSWORD"
+  prompt "Rancher namespace" "RANCHER_NAMESPACE" "$DEFAULT_RANCHER_NAMESPACE" "$ci"
+  prompt "Rancher Helm repository" "RANCHER_REPO" "$DEFAULT_RANCHER_REPO" "$ci"
+  prompt "Rancher password" "RANCHER_PASSWORD" "$DEFAULT_RANCHER_PASSWORD" "$ci"
 
-  local endpoints="$((echo "$K3S_URL $RANCHER_HOSTNAME"; hostname -I; hostname -A) | xargs -n1)"
+  local endpoints="$(echo "$public_ip $RANCHER_HOSTNAME $(hostname -I) $(hostname -A)" | xargs | tr ' ' ',')"
+  local install_k3s_cmd="curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION='$K3S_VERSION' sh -s - server --tls-san='$endpoints'"
 
-  # Propmt to user for confirmation
-  echo "Please confirm the following settings:"
+  if [ "$join" = 'true' ]; then
+    eval "$install_k3s_cmd"
+    return 0
+  fi
 
   hint "- Email: $LETSENCRYPT_EMAIL"
   hint "- K3s Version: $K3S_VERSION"
@@ -179,36 +227,14 @@ function main() {
   new_line
 
   echo "K3S Server will be accessible by these endpoints:"
-  echo "$endpoints" | xargs -I {} echo "  - {}"
+  echo "$endpoints" | xargs -d, -n1 echo '  -'
 
-  confirm "Do you want to continue?" "y"
-
-  local KUBECONFIG_FILE="/etc/rancher/k3s/k3s.yaml"
-
-  cat <<EOF >>"$PROFILE_FILE"
-export KUBECONFIG="$KUBECONFIG_FILE"
-EOF
-
-  export KUBECONFIG="$KUBECONFIG_FILE"
-
-  local k3s_tls_san="$(echo $endpoints | tr ' ' ',')"
-
-  # Install k3s
-  if [ -n "$K3S_TOKEN" ]; then
-    if [ -n "$K3S_URL" ]; then
-      echo "Your environment exposed a K3S_TOKEN and K3S_URL, using them to join the cluster."
-      curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="$K3S_VERSION" sh -s - server --server "$K3S_URL" --tls-san="$k3s_tls_san"
-
-      echo "Joined to the cluster with the provided token"
-      return 0
-    else
-      echo "Your environment exposed a K3S_TOKEN, using it to launch a high availability cluster."
-      curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="$K3S_VERSION" sh -s - server --cluster-init --tls-san="$k3s_tls_san"
-    fi
-  else
-    echo "Installing k3s with version $K3S_VERSION"
-    curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="$K3S_VERSION" sh -s - server --cluster-init --tls-san="$k3s_tls_san"
+  if [ "$ci" = 'false' ]; then
+    confirm "Do you want to continue?" "y"
   fi
+
+  echo "Installing k3s with version $K3S_VERSION"
+  eval "$install_k3s_cmd --cluster-init"
 
   # Install Helm
   curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
@@ -224,10 +250,7 @@ EOF
   kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.3/cert-manager.crds.yaml
 
   # Install Cert-Manager
-  helm install cert-manager jetstack/cert-manager \
-    --version v1.11.3 \
-    --namespace cert-manager \
-    --create-namespace
+  helm install cert-manager jetstack/cert-manager --version v1.11.3 --namespace cert-manager --create-namespace
 
   kubectl create namespace "$RANCHER_NAMESPACE"
 
@@ -280,7 +303,7 @@ EOF
   fi
 
   echo "You can also use this script to join new nodes to local cluster:"
-  echo "curl -sfL https://get.k3s.io | K3S_TOKEN="$k3s_server_token" sh -s - server --server https://$k3s_endpoint:6443"
+  echo "curl -sfL https://get.k3s.io | sh -s - server --token='$k3s_server_token' --server https://$k3s_endpoint:6443"
 }
 
 # Check if user is root
