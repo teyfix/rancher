faf796bef04094d859f1ff30885db310cabbe287
Halil Teyfik Dolmaci <hlltvfk@gmail.com>
2024-08-10 05:00:35 +0300

    fix: prompt asks for value even though it should seed

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ function prompt() {
   if [ "$should_seed" = "true" ]; then
     eval export $varname="$default"
     hint "\"$varname\" will be set to \"$default\""
+    return
   fi
 
   new_line
